ad121c6ca935e35132e38debd0e21e01657619de
hoanvipro <130170857+hoanvipro@users.noreply.github.com>
2024-08-19 22:21:19 +0700

    Update my_test.sv

--- CRC_ENCODER/test/my_test.sv
+++ CRC_ENCODER/test/my_test.sv
@@ -1,55 +1,3 @@
-//My agent
-class myAgent extends uvm_agent;
-  //Register to factory
-  `uvm_component_utils(myAgent)
-  //Create driver, sequencer and monitor
-  myDriver mydriver;
-  mySequencer mysequencer;
-  myMonitor mymonitor;
-  //Constructor
-  function new(string name = "myAgent", uvm_component parent);
-      super.new(name, parent);
-  endfunction
-  //Build phase
-    function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
-      //Factory registration
-      mydriver    = myDriver::type_id::create("mydriver",this);
-      mysequencer = mySequencer::type_id::create("mysequencer",this);
-      mymonitor = myMonitor::type_id::create("myMonitor",this);
-  endfunction
-	//Connect phase
-  function void connect_phase(uvm_phase phase);
-      super.connect_phase(phase);
-      mydriver.seq_item_port.connect(mysequencer.seq_item_export);
-  endfunction
-  
-//My agent
-endclass: myAgent
-
-class myEnv extends uvm_env;
-  //Register to factory
-  `uvm_component_utils(myEnv)
-  myAgent myagent;
-  
-  //Constructor
-  function new (string name = "myEnv", uvm_component parent = null);
-		super.new(name,parent);
-  endfunction
-  
-  //build phase
-  function void build_phase(uvm_phase phase);
-		super.build_phase(phase);
-    //Factory registration
-		myagent = myAgent::type_id::create("myagent",this);
-	endfunction
-  
-  //Connect phase
-  function void connect_phase(uvm_phase phase);
-		super.connect_phase(phase);
-	endfunction
-  
-endclass: myEnv
 
 class myTest extends uvm_test;
   //Register to factory
@@ -58,6 +6,7 @@ class myTest extends uvm_test;
   //Create env and sequence
   myEnv myenv;
   mySequence OmySequence; 
+  virtual_seq virseq;
   
   //Constructor
   function new (string name = "myTest", uvm_component parent = null);
@@ -70,6 +19,7 @@ class myTest extends uvm_test;
     //Factory registration
     myenv = myEnv::type_id::create("myenv",this);
     OmySequence = mySequence::type_id::create("OmySequence",this);
+    virseq = virtual_seq::type_id::create("VirSequence",this);
   endfunction
   
   //Run phase
@@ -77,20 +27,23 @@ class myTest extends uvm_test;
     		super.run_phase(phase);
     phase.raise_objection(this);
 		fork
-      begin
-        OmySequence.start(myenv.myagent.mysequencer);
-      end
+//       begin
+      //  OmySequence.start(myenv.myagent.mysequencer);
+      //  OmySequence.start(myenv.vfagent.vfsequencer);
+          
+         // OmySequence.start(myenv.evnsequencer);
+          virseq.start(myenv.evnsequencer);
+//      end
 			begin
 				#1ms;
 				`uvm_error("TEST SEQUENCE", "TIMEOUT!!!")
 			end
-		join_any
+        join_any
 		disable fork;
 		phase.drop_objection(this);
           	endtask
   //
 endclass: myTest
-
 
 
 

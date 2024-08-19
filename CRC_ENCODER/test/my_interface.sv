6601398a0c45278840d67ecae8883a1a1d599307
hoanvipro <130170857+hoanvipro@users.noreply.github.com>
2024-08-19 22:18:54 +0700

    Update my_interface.sv

--- CRC_ENCODER/test/my_interface.sv
+++ CRC_ENCODER/test/my_interface.sv
@@ -3,8 +3,22 @@ interface dutInterface;
   logic clk;
   logic resetn;
   logic ctrlen;
-  logic [9:0] datain;
-  logic [4 :0] genPoly;
-  logic [3 : 0] crcSeq;
+  logic [DATAWIDTH - 1 : 0] datain;
+  logic [CRCWIDTH : 0] genPoly;
+  logic [CRCWIDTH - 1 : 0] crcSeq;
   logic crcready;
 endinterface
+
+interface vfInterface;
+  logic clk;   
+  logic rstN;   
+  logic ctrlEn; 
+  logic [DATAWIDTH-1:0] dataIn; 
+  logic [CRCWIDTH-1:0] genPoly; 
+  logic [CRCWIDTH-1:0] initXorValue;
+  logic refInEn;  
+  logic refOutEn;
+  logic [CRCWIDTH-1:0] finalXorValue;
+  logic [CRCWIDTH-1:0] crcOut;
+  logic crcReady;
+endinterface;
